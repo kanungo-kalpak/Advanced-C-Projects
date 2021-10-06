dd9b70556ae722eeaf7640bc7a92523b0131bb1a
Digbijoy08 <83782347+Digbijoy08@users.noreply.github.com>
2021-10-06 11:58:32 +0530

    Updated pointer.c
    Please go through the changes.

--- pointer.c
+++ pointer.c
@@ -1,5 +1,3 @@
-/*function pointer example in c.*/
-
 #include <stdio.h>
 
 //function: sum, will return sum of two
@@ -7,24 +5,25 @@
 
 int addTwoNumbers(int x, int y)
 {
-   
+    return x + y;
 }
 
 int main()
 {
-    ;
+    int a, b, sum;
 
-
-    
-    
+    //function pointer declaration
+    int (*ptr_sum)(int, int);   //Modified 
+    //function initialisation
     ptr_sum = &addTwoNumbers;
 
     a = 10;
     b = 20;
 
-  
+    //function calling
+    sum = (*ptr_sum)(a, b);
 
-    
+    printf("Sum is: %d\n", sum);
 
     return 0;
 }
