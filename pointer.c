c9e9f87179e02cd5479b5e09b6ec98e44bf0d4c4
Kalpak Kanungo <55938006+kanungo-kalpak@users.noreply.github.com>
2021-10-06 11:55:54 +0530

    Create pointer.c
    Here I have made some changes in the code.

---
+++ pointer.c
@@ -0,0 +1,30 @@
+/*function pointer example in c.*/
+
+#include <stdio.h>
+
+//function: sum, will return sum of two
+//integer numbers
+
+int addTwoNumbers(int x, int y)
+{
+   
+}
+
+int main()
+{
+    ;
+
+
+    
+    
+    ptr_sum = &addTwoNumbers;
+
+    a = 10;
+    b = 20;
+
+  
+
+    
+
+    return 0;
+}
